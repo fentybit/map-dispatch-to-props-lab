078f78b337ea3f940d23bcf1cfe4d6bf7dfebcf5
fentybit <fentymhall@gmail.com>
2021-04-11 18:03:25 -0700

    commit tree cee154733a74edd9caead40d87f5735e6cc10e90

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state);
   }
 
   render() {
@@ -32,14 +32,14 @@ export class RestaurantInput extends Component {
         <p>
           <input
             type="text"
-            onChange={(event) => this.handleOnNameChange(event)}
+            onChange={this.handleOnNameChange}
             id="name"
             placeholder="restaurant name" />
         </p>
         <p>
           <input
             type="text"
-            onChange={(event) => this.handleOnLocationChange(event)}
+            onChange={this.handleOnLocationChange}
             id="location"
             placeholder="location" />
         </p>
@@ -49,6 +49,10 @@ export class RestaurantInput extends Component {
   }
 };
 
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: (newRestaurant) => dispatch(addRestaurant(newRestaurant))
+  }
+}
 
-//connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, mapDispatchToProps)(RestaurantInput);
